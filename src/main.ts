790f08a8039b0e7da9f86dce978cef8e24f03e09
xiangxn <xiangxn@163.com>
2025-11-25 22:13:24 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -96,8 +96,8 @@ async function main() {
     }
 
     // 清除控制台
-    // console.clear()
-    // manager.hook()
+    console.clear()
+    manager.hook()
     // 加载 positions
     await initPositions();
     await initOrderQueue();
@@ -107,7 +107,7 @@ async function main() {
 
     // 每秒刷新 UI
     while (running) {
-        // renderTable();
+        renderTable();
         await sleep(1)
     }
 }
