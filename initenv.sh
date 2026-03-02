4350a3c48238c084eaf54c8b5edb8991a4d896e2
xiangxn <xiangxn@163.com>
2026-03-02 22:03:38 +0800

    fix bug for script

--- initenv.sh
+++ initenv.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash && \
-source ~/.bashrc && \
-nvm install v22.13.1 && \
-corepack enable && \
+# load nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# install node
+nvm install v22.13.1
+
+# enable corepack
+corepack enable
+
+# install yarn
 yarn
